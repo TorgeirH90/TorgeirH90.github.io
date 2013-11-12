c649bdc901c030a9f5f4cb50efab69b9a3ab8106
2rgeir <2rgeir@gmail.com>
2013-11-12 16:40:09 +0100

    First try on website on github

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
 console.log('This would be the main JS file.');
+
+
+
+$(function () {
+    $("#Topshelf").load("MasterSite/TopBar.htm");
+    
+});
+
+/*document.open()
+{
+    alert("Hey1!");
+}*/
+
+function WAT() {
+    
+}
+
+function ORLY() 
+{
+    //Dostuff
+}
+
+//$(window).complete(WAT());
+//$(document).complete(ORLY());
+
+
